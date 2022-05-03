b9fd428177015d8f7bc7340226528603e59c2266
SteevesSaillant <steevessaillant@gmail.com>
2022-05-03 08:20:39 -0400

    1st

--- src/it/java/org/example/StepsDefs/LoginSteps.java
+++ src/it/java/org/example/StepsDefs/LoginSteps.java
@@ -1,14 +1,11 @@
 package org.example.StepsDefs;
 
 import io.cucumber.datatable.DataTable;
-import io.cucumber.datatable.DataTableTypeRegistryTableConverter;
 import org.example.App;
 import org.example.User;
 import org.junit.Assert;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
@@ -23,7 +20,7 @@ public class LoginSteps implements io.cucumber.java8.En {
         });
 
         Given("I user {string} with the password {string} and the {string} role", (String username, String password, String role) -> {
-            User actual = this.application.CreateNew(username,password,role);
+            User actual = this.application.CreateNewUser(username,password,role);
             User expected = new User(username,password,role);
             assertThat(actual).usingRecursiveComparison()
                     .isEqualTo(expected);
@@ -31,12 +28,15 @@ public class LoginSteps implements io.cucumber.java8.En {
 
         When("I submit my credentials as", (DataTable dataTable) -> {
             List<List<String>> rows = dataTable.asLists(String.class);
-            rows.subList(1,rows.size()).forEach(row -> {
-                if (!row.isEmpty() && row.) {
-                    //String columnName = row.remove(0);
-                    System.out.println(row.get(0).toCharArray());
-                }
-            });
+
+            dataTable.cells()
+                    .stream()
+                    .skip(1)        // Skip header row
+                    .map(fields -> {
+                        return application.CreateNewUser(fields.get(0), fields.get(1), fields.get(2));
+                    });
+                   // .forEach(application::CreateNewUser);
+               System.out.println(application.Users.get(0).username);
         });
         Then("{string} as {string}", (String string, String string2) -> {
             // Write code here that turns the phrase above into concrete actions
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,19 +1,23 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    public List<User> Users = new ArrayList<>();
+    public User CreateNewUser(String username, String password, String role) {
 
-    public User CreateNew(String username, String password,String role) {
+        User user;
 
-
-        org.example.User user;
         switch (username){
             case "admin":
                 user = new User(username,password,role);
+                this.Users.add(user);
                 break;
             default:
                 user = null;
