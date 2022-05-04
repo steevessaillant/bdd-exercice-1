f7b9b7438d8b8e5a0df17d1fe75f3e8f3e9245dd
SteevesSaillant <steevessaillant@gmail.com>
2022-05-04 15:32:57 -0400

    add maven-surefire-report-plugin for junit test reporting

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -3,6 +3,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class) @CucumberOptions(
+        plugin = { "me.jvt.cucumber.report.PrettyReports:target/cucumber" },
         features ="src/it/resources",
         glue = {"org.example.StepsDefs"},
         monochrome = true )
