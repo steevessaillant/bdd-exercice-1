8cc6986d292ee09305592e9a92a48138880985e6
SteevesSaillant <steevessaillant@gmail.com>
2022-05-13 12:04:03 -0400

    remove attribute

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -2,12 +2,10 @@ package org.example;
 
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class User {
     private boolean isAuthenticated;
     private Role role;
